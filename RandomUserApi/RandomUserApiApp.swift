41a054af8e3b5939745d262bf2ab98f1b03024c8
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-30 05:03:21 +0900

    Initial Commit

---
+++ RandomUserApi/RandomUserApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApiApp.swift
+//  RandomUserApi
+//
+//  Created by minkyuLee on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
